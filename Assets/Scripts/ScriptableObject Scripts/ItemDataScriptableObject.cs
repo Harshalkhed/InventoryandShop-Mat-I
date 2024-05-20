97d3303469b9e6b3ca6f0a741401f966b77923a0
Harshal S Khedekar <37449892+Harshalkhed@users.noreply.github.com>
2024-05-20 15:24:09 +0530

    Item Data Added and Inventory UI

---
+++ Assets/Scripts/ScriptableObject Scripts/ItemDataScriptableObject.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewItemData", menuName = "Data/NewItemData")]
+public class ItemDataScriptableObject : ScriptableObject
+{
+    public List<ItemData> items;
+
+    public ItemData GetItemData(string name)
+    {
+        return items.Find(itemdata => itemdata.itemName == name);
+    }
+
+    public ItemData GetRandomItemData()
+    {
+        return items[Random.Range(0, items.Count)];
+    }
+}
+
+[System.Serializable]
+public struct ItemData
+    {
+        public string itemName;
+        public Sprite icon;
+        [TextArea]
+        public string description;
+        public int buyingprice;
+        public int sellingprice;
+        public float weight;
+        public ItemType itemType;
+        public Rarity rarity;
+        public int quantity;
+        public bool isStackable;
+        public int maxStack;
+    }
+
+    public enum ItemType
+    {
+        Materials,
+        Weapons,
+        Consumables,
+        Treasure
+    }
+
+    public enum ItemStatus
+    {
+        Buying, Selling
+    }
+
+    public enum Rarity
+    {
+        VeryCommon,
+        Common,
+        Rare,
+        Epic,
+        Legendary
+    }
+
