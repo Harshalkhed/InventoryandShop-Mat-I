1b569e7aed6f0cfdec1ca697b4bebf1cc1d24312
Harshal S Khedekar <37449892+Harshalkhed@users.noreply.github.com>
2024-05-20 17:08:13 +0530

    Item Info Panel,Inventory Service,Game Service
    Item Info Panel created,Dummy info shown,Game Service Script and Inventory Script created
    ItemController UI script,Item ViewUI script

---
+++ Assets/Scripts/Item/ItemViewUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ItemViewUI : MonoBehaviour
+{
+    public Button itemButton;
+
+    [SerializeField] Image iconImage;
+    [SerializeField] TMP_Text quantityText;
+    private ItemControllerUI itemControllerUI;
+
+    public void SetController(ItemControllerUI itemController)
+    {
+        this.itemControllerUI = itemController;
+    }
+
+
+    public void Init(ItemData itemData)
+    {
+        this.iconImage.sprite = itemData.icon;
+        this.quantityText.text = $"X{itemData.quantity}";
+    }
+
+}
